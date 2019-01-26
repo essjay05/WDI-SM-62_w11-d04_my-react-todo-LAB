7e360316aa65a277edf5cd071ff62c3ac323d931
Joy <stepjoyous@gmail.com>
2019-01-25 16:27:18 -0800

    Updated App.js page to include routes for Home page and TodosContainer. Created Home component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import TodosContainer from './containers/TodosContainer';
 
 export default class App extends Component {
 
 	render() {
 		return (
 		<div className="container">
-			<h1>Hello World!</h1>
+			<Switch>
+				<Route exact path='/' component={ Home }/>
+				<Route path='/todos' component={ TodosContainer }/>
+			</Switch>
 		</div>
 		)
 	}
---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <h2>
+                This is the HOME page.
+            </h2>
+        );
+    }
+}
