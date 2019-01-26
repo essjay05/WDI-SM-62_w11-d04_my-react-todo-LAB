0e2d018347379d5399bfbc4b8217ac06898c5b90
Joy <stepjoyous@gmail.com>
2019-01-25 17:02:30 -0800

    Created container folder for TodosContainer

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../components/Home';
-import TodosContainer from '../components/TodosContainer';
+import TodosContainer from '../containers/TodosContainer';
 
 export default (
     <Switch>
