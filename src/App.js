59ce4d6d8b8cc16441a8bf8c771604ec74e138ee
Joy <stepjoyous@gmail.com>
2019-01-25 16:42:57 -0800

    Created Successful header showing up on main page and created route links on index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './components/Home';
 import TodosContainer from './components/TodosContainer';
 
@@ -8,6 +9,7 @@ export default class App extends Component {
 	render() {
 		return (
 		<div className="container">
+			<Header />
 			<Switch>
 				<Route exact path='/' component={ Home }/>
 				<Route path='/todos' component={ TodosContainer }/>
